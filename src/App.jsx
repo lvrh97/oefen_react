5fb601fa0a6294635feacea843f89d662c5982d0
lvrh97 <li.verheij@gmail.com>
2025-05-02 01:37:42 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Counter from './components/counter'
+
+function App() {
+
+
+  return (
+    <>
+    <div class='container-fluid'>
+    <div class='row justify-content-lg-center'>
+    <div class ='col-8'>
+    <Counter />
+    </div>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/counter.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './counter.css'
+
+
+
+
+export default function Counter(){
+    const [Count, setCount] = useState(0)
+
+
+    function increaseCount () {
+        setCount(prevCount => prevCount + 1)
+    }
+
+    return(
+      
+        <button class='btn btn-primary p-2 m-4' onClick={increaseCount}>You clicked the button {Count} times!</button>
+      
+    )
+}
