92c8a75f2a2875070b90019c036809f21effd6cd
lvrh97 <li.verheij@gmail.com>
2025-05-02 18:35:42 +0200

    Update contents.jsx

--- src/components/contents.jsx
+++ src/components/contents.jsx
@@ -5,9 +5,9 @@ import Navbar from "./navbar"
 export default function Contents (){
     return(
         <>
-        <div className="grid grid-cols-7 gap-5 ">
-            <div className="bg-[#E17000] col-span-7 text-white h-10/10 flex justify-center text-4xl shadow-md">Dashboard</div>
-            <div className="col-span-5 col-start-3 flex justify-center shadow-md h-200 mr-4" > <Map /> </div>
+        <div className="grid grid-cols-7 gap-5 content-evenly   h-lvh">
+            <div className="bg-[#E17000] col-span-7 text-white h-15  flex justify-center content-center text-4xl">Dashboard</div>
+            <div className="col-span-7 md:col-span-5 md:col-start-2 flex justify-center shadow-lg h-200 m-4" > <Map /> </div>
         </div>
         
         </>
