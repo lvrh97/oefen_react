8192c646bf52c62092ce9b6f384adf1f618c3c78
lvrh97 <li.verheij@gmail.com>
2025-05-06 09:31:51 +0200

    resize

--- src/components/contents.jsx
+++ src/components/contents.jsx
@@ -4,10 +4,9 @@ import Map from "./map"
 export default function Contents (){
     return(
         <>
-        <div className="grid grid-cols-12 gap-5  content-normal  h-lvh bg-[#F1F5F9]">
-            <div className="flex justify-center col-span-4"><image height='100px' href="../assets/LOGO_RO_NL_RGB_BASIS_BLAUW.svg"/></div>
-            <div className="bg-[#E17000] col-span-12 text-white p-3 h-15 flex justify-center content-center text-4xl">Dashboard</div>
-            <div className="col-span-10 col-start-2 lg:col-span-4 lg:col-start-8 flex justify-center shadow-lg h-200" > <Map /> </div>
+        <div className="grid grid-cols-12   ">
+            <div className="bg-[#E17000] col-span-12 text-white  h-20 flex justify-center  text-4xl items-center ">Dashboard</div>
+            <div className="col-span-10 col-start-2 lg:col-span-4 lg:col-start-8 flex justify-center shadow-lg h-120 lg:h-200 mt-10 " > <Map /> </div>
         </div>
         
         </>
